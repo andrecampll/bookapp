b6f36f894201633c327f9c55c258ce8027148988
André <andrevictor50@gmail.com>
2021-05-19 05:54:12 -0300

    Adding Responsiveness adjusts

--- src/components/DiscoverSection/Card.tsx
+++ src/components/DiscoverSection/Card.tsx
@@ -1,37 +1,46 @@
 import { Flex, Box, Heading, Text, Image } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 
 import { BiBarChartSquare } from 'react-icons/bi';
 
 export function Card() {
   return (
-    <Flex
-      bgColor="blue.800"
-      py="4"
-      px={["5", ""]}
-      borderRadius="5"
-      justify="space-between"
-      minW="71%"
-      maxH={["139", "200"]}
-      h={["100%", "200"]}
-      mt="4"
-      ml={["5", "10"]}
+    <motion.div
+      animate={{ scale: 1 }}
+      transition={{ duration: 0.5 }}
     >
-      <Box h="100%">
-        <Heading color="white.100" fontFamily="Playfair Display">Hooked</Heading>
-        <Text color="white.100" fontStyle="italic">Nir Eyal</Text>
+      <Flex
+        bgColor="blue.800"
+        py="4"
+        px={["5"]}
+        borderRadius="5"
+        justify="space-between"
+        maxH={["139", "200"]}
+        h={["100%", "200"]}
+        mt="4"
+        sx={{
+          "&": {
+            scrollSnapAlign: 'start'
+          }
+        }}
+      >
+        <Box h="100%">
+          <Heading color="white.100" fontFamily="Playfair Display">Hooked</Heading>
+          <Text color="white.100" fontStyle="italic">Nir Eyal</Text>
 
-        <Flex fontSize={["10", "12"]} align="center" color="white.100" mt={["5", "20"]}>
-          <BiBarChartSquare />&nbsp;<strong>120+</strong>&nbsp;Read Now
-        </Flex>
-      </Box>
+          <Flex fontSize={["10", "12"]} align="center" color="white.100" mt={["5", "20"]}>
+            <BiBarChartSquare />&nbsp;<strong>120+</strong>&nbsp;Read Now
+          </Flex>
+        </Box>
 
-      <Box>
-        <Image
-          src="https://m.media-amazon.com/images/I/41q7gZyFigL.jpg"
-          maxH={["111", "160"]}
-          borderRadius="5"
-        />
-      </Box>
-    </Flex>
+        <Box>
+          <Image
+            src="https://m.media-amazon.com/images/I/41q7gZyFigL.jpg"
+            maxH={["111", "160"]}
+            borderRadius="5"
+          />
+        </Box>
+      </Flex>
+    </motion.div>
   );
 }
--- src/components/HeaderContent.tsx
+++ src/components/HeaderContent.tsx
@@ -1,27 +1,26 @@
-import { Container, Box, Image, Button } from "@chakra-ui/react";
+import { Flex, Box, Image, Button } from "@chakra-ui/react";
 import Link from 'next/link';
 
 import { FiArrowLeft } from 'react-icons/fi'
 
 export function HeaderContent() {
   return (
-    <Container pt="8" px="5" bgColor="sand.500" minH="282" w="100%">
+    <Box pt="8" px="5" bgColor="sand.500" minH="282" w="100%" m="0">
       <Link href="/">
         <Button as="a" p="0" bgColor="transparent">
           <FiArrowLeft size="20"/>
         </Button>
       </Link>
-      <Box position="absolute">
+      <Flex align="center" justify="center" width="100%">
         <Image
           src="https://m.media-amazon.com/images/I/41q7gZyFigL.jpg"
           h="234"
           borderRadius="5"
-          position="relative"
-          top="2"
-          left="58%"
+          position="absolute"
+          top="20"
           boxShadow="0px 10px 10px rgba(216, 216, 216, 0.5)"
         />
-      </Box>
-    </Container>
+      </Flex>
+    </Box>
   );
 }
